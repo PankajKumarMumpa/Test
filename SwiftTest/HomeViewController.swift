aad6dbeec2ac8bf0e1fbf6458074c76cda4f7fd8
PankajKumarMumpa <mumpa@envigos-MacBook-Pro.local>
2017-03-01 17:00:30 +0530

    ddsdsv

--- SwiftTest/HomeViewController.swift
+++ SwiftTest/HomeViewController.swift
@@ -35,7 +35,7 @@ class HomeViewController: BaseViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated cvcv.
     }
     
 
